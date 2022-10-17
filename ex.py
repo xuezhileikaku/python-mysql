18af2ee760d3a5aa43e99c540bc76ff6849fad92
yingbo cheng <chengyingbo88@163.com>
2022-10-17 21:54:46 +0800

    update

--- ex.py
+++ ex.py
@@ -6,7 +6,7 @@ import traceback
 
 def getIp():
     # iplist get agent ip
-    ip_url = 'http://api.getip.com/getip'
+    ip_url = 'http://api.tianqiip.com/getip'
     res = requests.get(ip_url)
     print(res.text)
     ip_json = json.loads(res.text)
@@ -19,7 +19,7 @@ def getIp():
 
 def getHead(request_length):
     return {"Content-Type": "application/json", "Content-Length": str(request_length),
-            "Host": "api.myip.com",
+            "Host": "api.test.com",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
             "Accept": "*/*", "Connection": "keep-alive"}
 
@@ -56,11 +56,11 @@ def insert_table(arr, post_json):
     mydb = mysql.connector.connect(
         host="localhost", user="root", passwd="", database="db")
     mycursor = mydb.cursor()
-    # sql = "INSERT INTO company_list (company_id, company_name,search_json,) VALUES (%s, %s,)"
+    # sql = "INSERT INTO list (id, name,search_json,) VALUES (%s, %s,)"
     # val = [arr['cid'], arr['name']]    
-    sql = "INSERT INTO company_list(company_id,company_name,search_json,from_page,create_at,status,content) \
-       VALUES ('%s', '%s', '%s','%d','%d','%d','%s')" % \
-       (arr['cid'], arr['name'],json.dumps(json.loads(post_json)),p,time.time(),1,json.dumps(arr))
+    sql = "INSERT INTO list(id,name,search_json,create_at,status,content) \
+       VALUES ('%s', '%s', '%s','%d','%d','%s')" % \
+       (arr['cid'], arr['name'],json.dumps(json.loads(post_json)),time.time(),1,json.dumps(arr))
     try:
         # mycursor.execute(sql, val)
         mycursor.execute(sql)
@@ -84,7 +84,7 @@ def update_table(arr, post_json):
     mydb = mysql.connector.connect(
         host="localhost", user="root", passwd="", database="db")
     mycursor = mydb.cursor()
-    sql = "UPDATE company_list SET search_json = '"+json.dumps(json.loads(post_json))+"',create_at= "+str(time.time())+",status= 1,content= '"+json.dumps(arr)+"' WHERE company_name = '" + \
+    sql = "UPDATE list SET search_json = '"+json.dumps(json.loads(post_json))+"',create_at= "+str(time.time())+",status= 1,content= '"+json.dumps(arr)+"' WHERE name = '" + \
         arr['name']+"'"
 
     try:
@@ -107,7 +107,7 @@ def sele_table(arr):
     mydb = mysql.connector.connect(
         host="localhost", user="root", passwd="", database="db")
     mycursor = mydb.cursor()
-    sql = "SELECT * FROM company_list WHERE company_name='"+arr['name']+"'"
+    sql = "SELECT * FROM list WHERE name='"+arr['name']+"'"
 
     try:
         mycursor.execute(sql)
